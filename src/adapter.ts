d867b6cb87f98eaafb197eb6ba64b81715402633
Fabio Silva <fabio.silva@achieve3000.com>
2021-10-22 16:09:21 -0400

    K8S-29 - Debug Unknow value for question

--- src/adapter.ts
+++ src/adapter.ts
@@ -31,7 +31,7 @@ export class StorageAdapter extends TerminalAdapter {
 
   private findAnswer<Q, A>(name: string, question: Question<Q>): A {
     if (!this.answers.hasOwnProperty(name) && !question.default) {
-      this.console.log('findAnswer', name, question)
+      console.log('findAnswer', name, question)
       throw new Error(`Unknow value for question ${name}`)
     }
 
